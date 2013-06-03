e5dba70828963ebc0d54e161c0f6e125adce1d8d
rtoip91 <martoip123@gmail.com>
2013-06-04 01:31:38 +0200

    Do poprawy dodawnie użytkownika do bazy

--- panel2/src/java/Beans/MieszkaniecBean.java
+++ panel2/src/java/Beans/MieszkaniecBean.java
@@ -4,6 +4,9 @@
  */
 package Beans;
 import entities.Mieszkaniec;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  *
@@ -11,7 +14,45 @@ import entities.Mieszkaniec;
  */
 public class MieszkaniecBean {
 
-    Mieszkaniec mieszkaniec;
+  
+    private Mieszkaniec mieszkaniec = new Mieszkaniec();
+
+    public Mieszkaniec getMieszkaniec() {
+        return mieszkaniec;
+    }
+
+    
+    public void setMieszkaniec(Mieszkaniec mieszkaniec) {
+        this.mieszkaniec = mieszkaniec;
+    }
+
+    public static void setEmf(EntityManagerFactory emf) {
+        MieszkaniecBean.emf = emf;
+    }
+
+    public static EntityManagerFactory getEmf() {
+        return emf;
+    }
+    
+     private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("Jeju");
+     
+    public String dodaj (Mieszkaniec a)
+    {
+        this.mieszkaniec=a;
+        mieszkaniec.setUprawnienieID(1);
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        try
+        {
+         em.persist(mieszkaniec);
+         em.getTransaction().commit();
+         return null;
+        }
+        catch (Exception e)
+           {
+                    return "Nie powiodło się";
+            }
+    }
     /**
      * Creates a new instance of MieszkaniecBean
      */
