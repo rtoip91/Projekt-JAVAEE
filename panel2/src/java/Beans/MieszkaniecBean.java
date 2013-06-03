4da6671b130ace4f935476cec38ea1c17aed4e5d
rtoip91 <martoip123@gmail.com>
2013-06-04 00:46:53 +0200

    Ufff w końcu cos mam

---
+++ panel2/src/java/Beans/MieszkaniecBean.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Beans;
+import entities.Mieszkaniec;
+
+/**
+ *
+ * @author Piotrek
+ */
+public class MieszkaniecBean {
+
+    Mieszkaniec mieszkaniec;
+    /**
+     * Creates a new instance of MieszkaniecBean
+     */
+    public MieszkaniecBean() {
+    }
+}
