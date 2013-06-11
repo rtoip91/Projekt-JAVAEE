dba101fa96eda619c4e4cf340c33c581697dd6f1
rtoip91 <martoip123@gmail.com>
2013-06-11 12:55:14 +0200

    mojeee

--- panel2/src/java/Beans/BudynekBeans.java
+++ panel2/src/java/Beans/BudynekBeans.java
@@ -4,16 +4,43 @@
  */
 package Beans;
 import entities.Budynek;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  * @author Piotrek
  */
 public class BudynekBeans {
 
-    
+    Budynek budynek;
+
+    public Budynek getBudynek() {
+        return budynek;
+    }
+
+    public void setBudynek(Budynek budynek) {
+        this.budynek = budynek;
+    }
     /**
      * Creates a new instance of BudynekBeans
      */
     public BudynekBeans() {
+        budynek = new Budynek();
+        
+       
+    }
+    
+    public String dodaj ()
+    {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("PanelPU");
+        EntityManager em = emf.createEntityManager();
+        
+         em.getTransaction().begin();
+         budynek.setId(null);
+         em.persist(budynek);
+         em.getTransaction().commit();
+          em.close(); 
+         return null;
     }
 }
