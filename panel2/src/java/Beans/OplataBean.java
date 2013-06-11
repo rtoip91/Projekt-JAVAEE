659d5629e6bc10a2ccd640073db4ede9fb7820e4
tomzgar <t.zgarda@ymail.com>
2013-06-11 12:56:52 +0200

    opłaty

---
+++ panel2/src/java/Beans/OplataBean.java
@@ -0,0 +1,50 @@
+package Beans;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import entities.Oplata;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author Tomek
+ */
+
+public class OplataBean{
+
+    private Oplata oplata;
+
+    /**
+     * Creates a new instance of OplataBean
+     */
+    public OplataBean() {
+        oplata = new Oplata();
+    }   
+    
+        public Oplata getOplata() {
+        return oplata;
+    }
+        
+    public void setOplata(Oplata oplata){
+        this.oplata = oplata;
+    }    
+    
+    public String dodaj()
+    {
+       EntityManagerFactory emf = Persistence.createEntityManagerFactory("PanelPU");
+       
+       EntityManager em = emf.createEntityManager();
+       
+       em.getTransaction().begin();
+       oplata.setId(null);
+       em.persist(oplata);
+       em.getTransaction().commit();
+       em.close(); 
+       return null;       
+    }       
+}
