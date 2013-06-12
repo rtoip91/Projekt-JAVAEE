7061b738584566108fb4e5c224a178f9effe3526
tomzgar <t.zgarda@ymail.com>
2013-06-12 08:50:15 +0200

    Dodawanie sprawy

--- panel2/src/java/Beans/SprawaBean.java
+++ panel2/src/java/Beans/SprawaBean.java
@@ -3,6 +3,7 @@
  * and open the template in the editor.
  */
 package Beans;
+import entities.Mieszkaniec;
 import entities.Sprawa;
 import java.util.List;
 import javax.persistence.EntityManager;
@@ -14,20 +15,66 @@ import javax.persistence.Persistence;
  * @author janek
  */
 public class SprawaBean {
+    
+    private String login;
+    private Sprawa sprawa;
 
     /**
      * Creates a new instance of SprawaBean
      */
     public SprawaBean() {
+        sprawa = new Sprawa();
     }
-    
-            public List<Sprawa> getLista()
+
+    public Sprawa getSprawa() {
+        return sprawa;
+    }
+
+    public void setSprawa(Sprawa sprawa) {
+        this.sprawa = sprawa;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }   
+    public List<Sprawa> getLista()
     {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("panel2PU2");
         EntityManager em = emf.createEntityManager();
         List lista = em.createNamedQuery("Sprawa.findAll").getResultList();
         em.close();
-        return lista;
-        
+        return lista;  
+    }
+    private Mieszkaniec findMiesz()
+    {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("panel2PU2");
+        EntityManager em = emf.createEntityManager();
+        Mieszkaniec b =(Mieszkaniec) em.createNamedQuery("Mieszkaniec.findByLogin").setParameter("login", login).getSingleResult();
+        em.close();
+        return b;
+    }
+    public String dodaj ()
+    {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("panel2PU2");
+        EntityManager em = emf.createEntityManager();
+        Mieszkaniec a=findMiesz();
+        try{
+         em.getTransaction().begin();
+         sprawa.setMieszkaniecID(a);
+         sprawa.setId(null);
+         
+         em.persist(sprawa);
+         em.getTransaction().commit();
+         em.close(); 
+         }
+         catch (Exception e)
+             {
+                   em.close();   
+             }
+         return null;
     }
 }
